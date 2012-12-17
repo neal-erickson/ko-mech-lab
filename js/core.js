7ec3a4afb13cd8deba476c8c4c06efb627ba2721
Neal Erickson <minus.twelve@gmail.com>
2012-12-16 22:02:03 -0700

    added mwo xml files

--- js/core.js
+++ js/core.js
@@ -19,10 +19,42 @@
 			"CN9-A"
 		]);
 
+		self.activeMech = {
+			name: 'Hunchback HBK-4P',
+			tonnage: 50,
+			engine: { },
+			slots: [
+				rightArm: {
+					energyHardpoints: 1,
+					ballisticHardpoints: 0,
+					missileHardpoints: 0,
+					criticalSlots: 10
+				}
+			]
+		};
+
+		self.weapons = ko.observableArray([
+			{
+				name: 'Medium Laser',
+				weight: 1,
+				heat: 8,
+				criticals: 1,
+				type: 'energy' 
+			},
+			{ 
+				name: 'Large Laser',
+				weight: 5,
+				heat: 10,
+				criticals: 2,
+				type: 'energy'
+			}
+		]);
+
 		// Upgrade settings
 		self.structure = ko.observable('standard');
 		self.armor = ko.observable('standard');
 		self.heatSinks = ko.observable('single');
+		self.artemis = ko.observable('none');
 
 		// Calculated values
 		self.finalArmor = ko.computed(function() {
